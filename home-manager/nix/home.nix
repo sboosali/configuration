88714015c1a2056d5b99a247c11aad38b0e6783e
Sam Boosalis <samboosalis@gmail.com>
2018-10-14 23:16:58 -0700

    [home-manager] +sboo.systemPrograms

--- home-manager/nix/home.nix
+++ home-manager/nix/home.nix
@@ -1,55 +1,222 @@
+# 
 # cd ~/.config/nixpkgs/ && ln -sf ~/configuration/home-manager/nix/home.nix home.nix
 
-########################################
-# ①  Input #############################
-########################################
+##################################################
+# ①  Input #######################################
+##################################################
 
 { pkgs
-, ... 
+
+# , haskellPackages   ? (pkgs.haskellPackages)
+# , haskellCompilers  ? (pkgs.haskell.compiler)
+# 
+# , emacsPackages     ? (pkgs.emacs26Packages)
+# , melpaPackages     ? (pkgs.emacs26PackagesNg)
+
+, ...
 }:
 
-########################################
-# ②  Utilities #########################
-########################################
+##################################################
+# ②  Utilities ###################################
+##################################################
 let
-########################################
+##################################################
 
-sboo = {
+inherit (pkgs) lib;
 
-  sanfrancisco = {
-    longitude = "38";
-    latitude  = "122";
-  };
+##################################################
 
-  boston = {
-    longitude = "42";
-    latitude  = "71";
-  };
+haskellPackages   = (pkgs.haskellPackages);
+
+haskellCompilers  = (pkgs.haskell.compiler);
+
+##################################################
+in
+##################################################
+# ②  Utilities: sboo #############################
+##################################################
+let sboo = rec {
+##################################################
+
+paths = {
+
+  home-manager = https://github.com/rycee/home-manager/archive/release-18.03.tar.gz;
 
 };
 
-########################################
+##################################################
 
-# = ;
+config = {
 
-########################################
-in
-########################################
-{
-########################################
-# ③  Packages ##########################
-########################################
 
-home.packages = [
 
-    pkgs.htop
-    pkgs.fortune
+};
+
+##################################################
+
+programs = freePrograms
+#       ++ unfreePrograms
+         ;
+
+##################################################
+
+freePrograms = builtins.concatLists [
+
+  systemPrograms
+  haskellPrograms
+  haskellCompilerPrograms
+  xorgPrograms
 
 ];
 
+# systemPrograms ++ haskellPrograms ++ haskellCompilerPrograms ++ xorgPrograms;
+
+##################################################
+
+unfreePrograms = with pkgs; [
+
+ google-chrome
+
+];
+
+##################################################
+
+systemPrograms = with pkgs; [
+
+ bash-completion
+ cabal-install
+ cabal2nix
+ #cask
+ colordiff
+ #dconf-editor
+ dex
+ dhall
+ #emacs
+ #emacs2nix
+ #melpa2nix
+ expect
+ feh
+ flac
+ fltk
+ #fsnotify
+ fswatch
+ ghc
+ gimp
+ glib
+ gparted
+ graphviz
+ htop
+ imagemagick
+ inotify-tools
+ jq
+ mesa
+ ncdu
+ #nix
+ #nix-derivation-pretty
+ nix-prefetch-git
+ nox
+ pandoc
+ pocketsphinx
+ postgresql
+ qbittorrent
+ shellcheck
+ signal-desktop
+ sox
+ stack
+ tmux
+ tree
+ unzip
+ #VirtualBox-GuestAdditions
+ vlc
+ wmctrl
+ youtube-dl
+ 
+ xautomation
+ #xbacklight
+ xbindkeys
+ xbrightness
+ xcalib
+ xclip
+ xdg_utils
+ xdotool
+ #xinput
+ xinput_calibrator
+ xkbmon
+ #xmodmap
+ #xprop
+ xrandr-invert-colors
+ #xsel-unstable
+ xtitle
+ xvkbd
+
+];
+
+# ^ NOTE we must omit the `programs._` programs:
+#
+# * emacs26
+# * git
+# * firefox
+# * ...
+#
+# from the `home.packages = _` programs.
+#
+
 ########################################
-# ④  Programs ##########################
-########################################
+
+xorgPrograms = with pkgs.xorg; [
+
+ xprop
+
+];
+
+##################################################
+
+haskellPrograms = with haskellPackages; [
+
+ ghcid
+ git-annex
+
+];
+
+##################################################
+
+haskellCompilerPrograms = with haskellCompilers; [
+
+ ghcjs
+
+];
+
+##################################################
+
+locations = {
+
+  sanfrancisco.longitude = "38";
+  sanfrancisco.latitude  = "122";
+
+  boston.longitude = "42";
+  boston.latitude  = "71";
+
+};
+
+##################################################
+
+obfuscatedEmail = builtins.concatStringsSep "" ["sam" "boosalis" "@" "gmail" "." "com"];
+
+##################################################
+};
+
+in
+##################################################
+{
+##################################################
+# ③  Packages ####################################
+##################################################
+
+home.packages = sboo.programs;
+
+##################################################
+# ④  Programs ####################################
+##################################################
 
 programs.emacs = {
     enable = true;
@@ -69,30 +236,29 @@ programs.emacs = {
     ];
 };
 
-
-####################################
+##############################################
 
 programs.firefox = {
     enable = true;
 
-    enableIcedTea = true;
+    #enable = true;
 };
 
-####################################
+##############################################
 
  programs.git = {
     enable = true;
 
-    userName = "Sam Boosalis";
-    userEmail = "samboosalis@gmail.com";
+    userName  = "Sam Boosalis";
+    userEmail = sboo.obfuscatedEmail;
 };
 
-####################################
+##############################################
 
 programs.home-manager = {
     enable = true;
 
-    path = https://github.com/rycee/home-manager/archive/release-18.03.tar.gz;
+    path = sboo.paths.home-manager;
 };
 
 # ^ 
@@ -101,15 +267,15 @@ programs.home-manager = {
 #
 # 
 
-########################################
-# ⑤  Services ##########################
-########################################
+##################################################
+# ⑤  Services ###################################
+##################################################
 
 services.redshift = {
     enable = true;
 
     provider  = "manual";
-    inherit (sboo.boston) longitude latitude;
+    inherit (sboo.locations.boston) longitude latitude;
 
 #    provider  = "manual";
 #    longitude = "42";
@@ -118,15 +284,17 @@ services.redshift = {
     temperature.night  = 1000;
     temperature.day    = 20000;
 
-    # ^ min 1000 (red), max 25000 (blue)
+    # ^ kelvins.
+    # min 1000 (red), max 25000 (blue).
 
     brightness.night  = "0.9";
     brightness.day    = "1.0";
 
-    # ^ min 0.1 (dim, 10%), max 1.0 (bright, 100%)
+    # ^ fraction.
+    # min 0.1 (dim, 10%), max 1.0 (bright, 100%).
 };
 
-########################################
+##################################################
 
 #TODO# services.gpg-agent = {
 #    enable = true;
@@ -135,16 +303,16 @@ services.redshift = {
 #    enableSshSupport = true;
 #};
 
-########################################
+##################################################
 
 #TODO# services.ssh-agent = {
 #    enable = true;
 #
 # };
 
-########################################
-# ⑥ _ ##################################
-########################################
+##################################################
+# ⑥ _ ############################################
+##################################################
 
-########################################
+##################################################
 }
