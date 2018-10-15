a78c4d8bd5994930f3caf6dda01b1e8d897730bf
Sam Boosalis <samboosalis@gmail.com>
2018-10-14 23:15:46 -0700

    rm

--- argos/scripts/shell_log.1s.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-LOG_ENTRY=$(journalctl /usr/bin/gnome-shell -n 1 --output=cat --no-pager)
-echo "<span color='#9BF' weight='normal'><small><tt>$LOG_ENTRY</tt></small></span> | length=40"
-
-echo "---"
-echo "View GNOME Shell Log | bash='journalctl /usr/bin/gnome-shell -f'"
-
