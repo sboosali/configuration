0cc195f552c389f8c9c63ef86b3be6a676cac2da
Sam Boosalis <samboosalis@gmail.com>
2018-10-17 22:18:19 -0700

    [hm] `.ssh/config`

---
+++ home-manager/nix/home/ssh.nix
@@ -0,0 +1,70 @@
+##################################################
+{ pkgs
+
+, sboo
+
+, xdg
+}:
+##################################################
+let
+
+inherit (pkgs.stdenv) lib;
+
+##################################################
+
+sshFile = path:
+  ''${builtins.getEnv "HOME"}/.ssh/${path}'';
+
+# sshFile = path:
+#   "${xdg.configHome}/ssh/${path}";
+
+in
+##################################################
+let
+
+matchBlocks = rec
+{
+
+  ############################
+
+  "github.com" = {
+    user     = "git";
+    hostname = "github.com";
+
+    identityFile = sshFile "%l_%r@%h_id_rsa";  #TODO
+   #extraOptions = {
+   #     "_" = "_";
+   # };
+  };
+
+  ############################
+
+  #TODO# router = { hostname = "192.168.1.2"; user = "root"; };
+
+  #TODO# haskell_org = { host = "*haskell.org"; user = "sboo"; };
+
+  ############################
+
+  id_local = {
+    identityFile   = sshFile "id_local";
+    identitiesOnly = true;
+   #host           = lib.concatStringsSep " " [ ];
+  };
+
+  ############################
+};
+
+in
+##################################################
+{
+
+ forwardAgent        = true;
+ serverAliveInterval = 60;
+
+ hashKnownHosts      = true;
+ userKnownHostsFile  = sshFile "known_hosts";
+ 
+ inherit matchBlocks;
+
+}
+##################################################
