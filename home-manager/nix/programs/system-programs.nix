28d2a39f45a109c24baecb37fda119a0bb29a34c
Sam Boosalis <SamBoosalis@gmail.com>
2018-11-27 03:31:22 -0800

    add `system-programs.nix`

---
+++ home-manager/nix/programs/system-programs.nix
@@ -0,0 +1,93 @@
+##################################################
+{ pkgs }:
+
+##################################################
+
+with pkgs; 
+
+##################################################
+[
+
+ bash-completion
+ cabal-install
+ cabal2nix
+ colordiff
+ dex
+ dhall
+ expect
+ feh
+ flac
+ fltk
+ fswatch
+#ghc                                              # see ../haskell/compilers.nix
+ gimp
+ glib
+ gparted
+ graphviz                       # GraphViz renders « .dot » files.
+ htop
+ imagemagick
+ inotify-tools
+ jq
+ libreoffice
+ #mdless
+ multimarkdown
+ nano
+ ncdu
+ nix-bash-completions
+ nix-info
+ nix-prefetch-github
+ nix-prefetch-scripts
+ nixnote2                       # NixNote is an unofficial Evernote client.
+ nox
+ openssh
+ pandoc
+ pocketsphinx
+ postgresql
+ qbittorrent
+ redshift
+ shellcheck
+ signal-desktop
+ sox
+ stack
+ tmux
+ tree
+ unzip
+ vlc
+ wmctrl
+ xautomation
+ xbindkeys
+ xbrightness
+ xcalib
+ xclip
+ xdg_utils
+ xdotool
+ xinput_calibrator
+ xkbd
+ xkbmon
+ xrandr-invert-colors
+ xtitle
+ xvkbd
+ youtube-dl
+
+ #nixfmt
+ #terminfo
+ #VirtualBox-GuestAdditions
+ #cask
+ #dconf-editor
+ #emacs
+ #emacs2nix
+ #fsnotify
+ #melpa2nix
+ #mesa
+ #nix
+ #nix-derivation-pretty
+ #nix-prefetch-git            # subsumed within « nix-prefetch-scripts ».
+ #xbacklight
+ #xinput
+ #xkeyboard-config
+ #xmodmap
+ #xprop
+ #xsel-unstable
+
+]
+##################################################
