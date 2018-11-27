c0791935b87c251f2a9b65de168150bdcc98d277
Sam Boosalis <SamBoosalis@gmail.com>
2018-11-27 03:31:34 -0800

    add metadata/versions

---
+++ home-manager/nix/versions/home-manager.nix
@@ -0,0 +1,4 @@
+{
+ remote = https://github.com/rycee/home-manager/archive/release-18.09.tar.gz;
+ local  = ../../../submodules/home-manager;
+}
---
+++ home-manager/nix/versions/nixpkgs.nix
@@ -0,0 +1,4 @@
+{
+ remote = https://github.com/NixOS/nixpkgs/archive/18.09.tar.gz;
+#TODO local  = ../../../submodules/nixpkgs;
+}
