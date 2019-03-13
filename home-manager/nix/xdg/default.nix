01195e8cc9bd702cbdfc2e42604104dfa5e946f0
Sam Boosalis <SamBoosalis@gmail.com>
2019-03-13 15:11:22 -0700

    nix-ify « $PS1 »

--- home-manager/nix/xdg/default.nix
+++ home-manager/nix/xdg/default.nix
@@ -5,28 +5,29 @@
 
 ##################################################
 let
+#------------------------------------------------#
 
-env = {
+HOME = builtins.getEnv "HOME";
 
-  HOME = builtins.getEnv "HOME";
-
-};
-
+#------------------------------------------------#
 in
 ##################################################
 {
+ #-----------------------------#
 
- configHome = "${env.HOME}/.config";
- dataHome   = "${env.HOME}/.local/share";
- cacheHome  = "${env.HOME}/.cache";
+ configHome = "${HOME}/.config";
+ dataHome   = "${HOME}/.local/share";
+ cacheHome  = "${HOME}/.cache";
 
  # ^ the defaults.
 
-##################################################
+#TODO check XDG_*_HOME envvars (here or later)?
+
+ #-----------------------------#
 
  configFile = import ./config.nix { inherit sboo applications; };
+ dataFile   = import ./data.nix   { inherit sboo applications; };
 
- dataFile   = import ./data.nix { inherit sboo applications; };
-
+ #-----------------------------#
 }
 ##################################################
