a0cba1b2d983b320419cd8069c075dbc1ca79c4d
Sam Boosalis <SamBoosalis@gmail.com>
2019-03-12 21:56:40 -0700

    split « *-packages.ni »

---
+++ home-manager/nix/programs/javascript-programs.nix
@@ -0,0 +1,35 @@
+##################################################
+{ self }:
+
+##################################################
+
+with self; 
+
+##################################################
+let
+#------------------------------------------------#
+
+#------------------------------------------------#
+in
+##################################################
+[
+
+  npm                           # JS Package Manager
+  webpack                       # JS Build Tool
+  webpack-cli                   # JS Build Tool
+  eslint                        # JS Linter
+  jshint                        # JS Linter
+  prettier                      # JS Formatter
+  uglify-js                     # JS Minifier
+  typescript                    # JS Type System
+  jsonlint                      # JSON Linter
+  htmlhint                      # HTML Linter
+  textlint                      # Markdown Linter
+  markdown-link-check           # Markdown Linter
+  react-tools                   # React.js
+  react-native-cli              # React.js
+  create-react-app              # React.js
+  create-react-native-app       # React.js
+
+]
+##################################################
